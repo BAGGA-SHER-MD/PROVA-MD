637c30bf731b2ad916638793c489d7c02c7418e7
BAGGA-SHER-MD <kamran.md.bot@gmail.com>
2026-03-16 08:09:00 +0500

    Update config.js

--- config.js
+++ config.js
@@ -45,7 +45,7 @@ CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "🥰,🚀,💋,❤️�
 // chose custom react emojis by yourself 
 DELETE_LINKS: process.env.DELETE_LINKS || "false",
 // automatic delete links witho remove member 
-OWNER_NUMBER: process.env.OWNER_NUMBER || "923147168309",
+OWNER_NUMBER: process.env.OWNER_NUMBER || "923259855436",
 // add your bot owner number
 OWNER_NAME: process.env.OWNER_NAME || "PROVA-MD",
 // add bot owner name
